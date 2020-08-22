5c596b03e8eaea485446f329e71947fb55b0e36e
Alberto Correia <alberto_cpv@hotmail.com>
2020-08-23 01:43:34 +0200

    hotfix

--- src/Components/Providers/Providers.jsx
+++ src/Components/Providers/Providers.jsx
@@ -2,36 +2,38 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom'
+
 export class Products extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: []
+      providers: []
     };
   }
   componentDidMount() {
-    axios.get("http://localhost:3000/api/products/")
+    axios.get("http://localhost:3000/api/providers/")
         .then((response) => {
           this.setState({
-            products: response.data
+            providers: response.data
           });
         })
-        .catch((err) => console.log(" ESTE ES EL ERROR", err));
+        .catch((error) => console.log(" ESTE ES EL ERROR", error));
   }
   render() {
-    const products = this.state.products.map((product) => (
-      <div key={product._id}>
-        <h1><Link to={"/products/" + product._id}>{product.name}</Link></h1>
+    const providers = this.state.providers.map((provider) => (
+      <div key={provider._id}>
+        <h1><Link to={"/providers/" + provider._id}>{provider.name}</Link></h1>
       </div>
     ));
     return (
       <div className="container">
         <div style={{ width: "60%", float: "left" }}>
           <h1>Products List</h1>
-          {products}
+          {providers}
         </div>
       </div>
     );
   }
 }
+
 export default Products;
