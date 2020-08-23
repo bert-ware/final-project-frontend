5a1cd6f1c675972db6829872b3eae3e79eb86743
Robert <robert.anibal.ledezma@gmail.com>
2020-08-23 16:34:09 +0200

    Routes components fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
-import Home from './Components//Home/Home.jsx';
-import Products from './Components/Products/Products';
-import Providers from "./Components/Providers/Providers"
-import ProviderDetails from "./Components/ProviderDetails/ProviderDetails"
-import ProductDetails from "./Components/ProductDetails/ProductDetails"
+import ProductDetails from './components/ProductDetails/ProductDetails'
+import Products from './components/Products/Products';
+import Providers from "./components/Providers/Providers"
+import ProviderDetails from "./components/ProviderDetails/ProviderDetails"
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/'><Home/></Route>
+        <Route exact path='/'>
+            <Home/>
+        </Route>
         <Route exact path='/products'><Products/></Route>
-        <Route path="/products/:id" render={(props) => <ProductDetails {...props} />} />
+        <Route path="/products/:id" render={(props) => <ProductDetails  {...props}/>} />
         <Route exact path='/providers'> <Providers/></Route>
         <Route path="/providers/:id" render={(props) => <ProviderDetails {...props} />} />
       </Switch>
