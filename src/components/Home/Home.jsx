9193f14fc1674c62d6e2dd2186b6a7af1a247dc9
Alberto Correia <alberto_cpv@hotmail.com>
2020-08-28 20:28:43 +0200

    fix home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
-//import { Link } from "react-router-dom";
+import React, { Component } from "react"
+//import { Link } from "react-router-dom"
 import "./Home.css";
-import "bootstrap/dist/css/bootstrap.css";
-import Header from "./Header.jsx";
+import "bootstrap/dist/css/bootstrap.css"
+import Header from "./Header.jsx"
+import Navbar from "../Navbar/Navbar"
+import BottomNavigation from "../BottomNavigation/BottomNavigation"
 
 
 export class Home extends Component {
-<<<<<<< HEAD
   render() {
     return (
       <div>
-        <Header />
-
+        <Navbar/>
+        <Header/>
         <section id="intro">
           <h2>Lorem ipsum dolor</h2>
           <p>
@@ -33,42 +34,22 @@ export class Home extends Component {
 
           <div className="grid_container">
             <div className="box grid_item">
-              <img className='galerie-2' src="https://images.unsplash.com/photo-1510626176961-4b57d4fbad03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="img" />
+              <img className='galerie-2' src="https://scontent-mad1-1.xx.fbcdn.net/v/t31.0-8/28514692_1157581404377630_2337639456659598352_o.jpg?_nc_cat=110&_nc_sid=730e14&_nc_ohc=K3FxEP37DMIAX-Igpsi&_nc_ht=scontent-mad1-1.xx&oh=a3725101897dbca0e1d3f9eeca962925&oe=5F6D2D21" alt="img" />
             </div>
 
             <div className="box grid_item">
-              <img className='galerie-2' src="https://images.unsplash.com/photo-1510626176961-4b57d4fbad03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="img" />
+              <img className='galerie-2' src="https://scontent-mad1-1.xx.fbcdn.net/v/t31.0-8/15591630_10154805871668498_593361105097038447_o.jpg?_nc_cat=107&_nc_sid=9267fe&_nc_ohc=3aypFyyGPNoAX_rRlHm&_nc_ht=scontent-mad1-1.xx&oh=a1a032eb578d6a3cc2d3c6bd211a703f&oe=5F6F0BFF" alt="img" />
             </div>
             
             <div className="box grid_item">
-              <img className='galerie-2' src="https://images.unsplash.com/photo-1510626176961-4b57d4fbad03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="img" />
+              <img className='galerie-2' src="https://scontent-mad1-1.xx.fbcdn.net/v/t31.0-8/28619423_1158161937652910_2273450483933074375_o.jpg?_nc_cat=103&_nc_sid=730e14&_nc_ohc=heFaEG0tb-EAX88ys8Z&_nc_ht=scontent-mad1-1.xx&oh=447919e385123fb8e381805d9658510a&oe=5F6D5EF5" alt="img" />
             </div>
           </div>
         </section>
-
-        {/* <h1>Este es el Home</h1>
-               <h2> <Link to={"/products/" }>Products</Link></h2>
-               <h2> <Link to={"/providers/" }>Providers</Link></h2>
-               <h2> <Link to={"/recipes/" }>Recipes</Link></h2>
-                <BottomNavigation/> */}
+        <BottomNavigation/>
       </div>
-    );
+    )
   }
-=======
-    render() {
-        return (
-            <div>   
-                <h1>Este es el Home</h1>
-               <h2> <Link to={"/products/" }>Products</Link></h2>
-               <h2> <Link to={"/providers/" }>Providers</Link></h2>
-               <h2> <Link to={"/recipes/" }>Recipes</Link></h2>
-               <h2> <Link to={"/user-profile" }>user profile</Link></h2>
-                <SimpleBottomNavigation/>
-
-            </div>
-        )
-    }
->>>>>>> 97959ac4c4b26608c21f84f6e029c3f5aaad158a
 }
 
 export default Home;
