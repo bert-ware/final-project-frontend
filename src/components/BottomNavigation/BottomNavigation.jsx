bcb072f59a24d0a87868683623ad354baf97fa28
Robert <robert.anibal.ledezma@gmail.com>
2020-09-13 21:16:10 +0200

    Last dance bottom bar

--- src/components/BottomNavigation/BottomNavigation.jsx
+++ src/components/BottomNavigation/BottomNavigation.jsx
@@ -15,30 +15,66 @@ export class BottomNavigation extends Component {
       profileInactive: "/img/Profile.svg",
       providersInactive: "/img/Providers.svg",
       recipesInactive: "/img/Recipes.svg",
+      products: "/img/Products.svg",
+      productsActive: "/img/Products2.svg",
+      productsInactive: "/img/Products.svg",
+      textProfile: "white",
+      textRecipes: "white",
+      textProducts: "white",
+      textProviders: "white",
     };
   }
+
+  //Color changes
   handleClick = (event) => {
-    
     if (event.target.name === "profile") {
       this.setState({
         profile: this.state.profileActive,
         recipes: this.state.recipesInactive,
         providers: this.state.providersInactive,
+        products: this.state.productsInactive,
+        textProfile: "#15540e",
+        textRecipes: "white",
+        textProducts: "white",
+        textProviders: "white",
       });
     } else if (event.target.name === "providers") {
       this.setState({
         profile: this.state.profileInactive,
         recipes: this.state.recipesInactive,
         providers: this.state.providersActive,
+        products: this.state.productsInactive,
+        textProviders: "#15540e",
+        textProfile: "white",
+        textRecipes: "white",
+        textProducts: "white",
       });
     } else if (event.target.name === "recipes") {
       this.setState({
         profile: this.state.profileInactive,
         recipes: this.state.recipesActive,
         providers: this.state.providersInactive,
+        products: this.state.productsInactive,
+        textRecipes: "#15540e",
+        textProfile: "white",
+        textProducts: "white",
+        textProviders: "white",
+      });
+    } else if (event.target.name === "products") {
+      this.setState({
+        profile: this.state.profileInactive,
+        recipes: this.state.recipesInactive,
+        providers: this.state.providersInactive,
+        products: this.state.productsActive,
+        textProducts: "#15540e",
+        textProfile: "white",
+        textRecipes: "white",
+        textProviders: "white",
       });
     }
   };
+
+  
   render() {
     return (
       <div className="FooterNav">
@@ -53,7 +89,16 @@ export class BottomNavigation extends Component {
                 alt="profile icon"
               />
             </div>
-            Profile
+            <p
+              style={{
+                color: `${this.state.textProfile}`,
+                marginTop: "5px",
+                fontSize: "8px",
+                textDecoration: 'none'
+              }}
+            >
+              PROFILE
+            </p>
           </Link>
         </div>
 
@@ -65,11 +110,20 @@ export class BottomNavigation extends Component {
                 id="recipes"
                 name="recipes"
                 src={this.state.recipes}
-                key={'recipes'+this.state.recipes}
-                alt="profile icon"
+                key={"recipes" + this.state.recipes}
+                alt="recipes icon"
               />
             </div>
-            Recipes
+            <p
+              style={{
+                color: `${this.state.textRecipes}`,
+                marginTop: "5px",
+                fontSize: "8px",
+                textDecoration: 'none'
+              }}
+            >
+              RECIPES
+            </p>
           </Link>
         </div>
 
@@ -80,12 +134,21 @@ export class BottomNavigation extends Component {
                 onClick={this.handleClick}
                 id="products"
                 name="products"
-                src={this.state.recipes}
-                key={'products'+this.state.recipes}
-                alt="profile icon"
+                src={this.state.products}
+                key={"products" + this.state.products}
+                alt="product icon"
               />
             </div>
-            Products
+            <p
+              style={{
+                color: `${this.state.textProducts}`,
+                marginTop: "5px",
+                fontSize: "8px",
+                textDecoration: 'none'
+              }}
+            >
+              PRODUCTS
+            </p>
           </Link>
         </div>
 
@@ -97,11 +160,20 @@ export class BottomNavigation extends Component {
                 id="providers"
                 name="providers"
                 src={this.state.providers}
-                key={'providers'+this.state.providers}
+                key={"providers" + this.state.providers}
                 alt="profile icon"
               />
             </div>
-            Providers
+            <p
+              style={{
+                color: `${this.state.textProviders}`,
+                marginTop: "5px",
+                fontSize: "8px",
+                textDecoration: 'none'
+              }}
+            >
+              PROVIDERS
+            </p>
           </Link>
         </div>
       </div>
