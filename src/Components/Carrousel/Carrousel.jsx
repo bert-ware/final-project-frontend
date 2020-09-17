ac46d388a07936d901589cbc5ab7ff95a67c2569
Robert <robert.anibal.ledezma@gmail.com>
2020-09-17 12:49:33 +0200

    table done

--- src/Components/Carrousel/Carrousel.jsx
+++ src/Components/Carrousel/Carrousel.jsx
@@ -1,61 +1,19 @@
 import React, { Component } from 'react'
 //import React from 'react';
-import { render } from 'react-dom';
 import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
 import './Carrousel.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const content = [
-	{
-		title: 'DrinkApp',
-		// description:
-		// 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
-		image: './img/drinks/signupslider.png',
-	},
-	
-];
 
 
-
-// function Carrousel() {
-//     const infoSlider = content.map((item, index) => (
-//         <div
-//             key={index}
-//             className="slider-content"
-//             style={{ background: `url('${item.image}') no-repeat center center` }}
-//         >
-//             <div className="inner">
-//                 <h1>{item.title}</h1>
-//                 {/* <p>{item.description}</p> */}
-//             </div>
-        
-//         </div>
-//     ))
-
-
-
-
-//     return (
-//         	<div>
-
-// 		<Slider className="slider-wrapper">
-// 			{infoSlider}
-// 		</Slider>
-// 	</div>
-//     )
-// }
-
-// export default Carrousel
 
 
 
 
 
 export class Carrousel extends Component {
-    constructor(props){
-        super(props)
-    }
+
     render() {
             
         return (
--- src/Components/RecipesDetails/RecipeDetails.js
+++ src/Components/RecipesDetails/RecipeDetails.js
@@ -1,88 +1,166 @@
-import React, { Component } from 'react'
-import axios from "axios"
-import { Redirect } from 'react-router-dom'
-import "./RecipeDetails.css"
-import FileUploadRecipes from "../Fileupload/FileUploadRecipes"
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import "./RecipeDetails.css";
+import FileUploadRecipes from "../Fileupload/FileUploadRecipes";
+import Carrousel from "../Carrousel/Carrousel";
+
 
 export class RecipeDetails extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            id: props.match.params.id,
-            recipe: {
-                name: "",
-                ingredients: [],
-                method: ""
-            },
-            redirect: false,
-        }
-    }
-    //Recogida datos API
-    componentDidMount() {
-        axios.get("http://localhost:3000/api/recipes/" + this.state.id, {withCredentials: true})
-        .then(response => {
-            this.setState({
-                recipe: response.data, 
-            })
-        })
-    }
- //Handle borrar
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.match.params.id,
+      recipe: {
+        name: "",
+        ingredients: [],
+        method: "",
+      },
+      redirect: false,
+    };
+  }
+  //Recogida datos API
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/api/recipes/" + this.state.id, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          recipe: response.data,
+        });
+      });
+  }
+  //Handle borrar
   handleClick = () => {
-    axios.delete("http://localhost:3000/api/recipes/" + this.state.id, {withCredentials: true})
-    .then(() => this.setState({ redirect: true }))}
+    axios
+      .delete("http://localhost:3000/api/recipes/" + this.state.id, {
+        withCredentials: true,
+      })
+      .then(() => this.setState({ redirect: true }));
+  };
 
-    render() {
-        //Map ingredients
-       const ingredients = this.state.recipe.ingredients.map((ingredient) =>
-            <li key={ingredient.product._id}> {ingredient.product.name} </li>
-        )
-      
-        //Map mesures
-        const mesures = this.state.recipe.ingredients.map((ingredient, index) =>
-            <li key={index}> {ingredient.quantity} {ingredient.product.typeFormat}</li>
-        )
-       //Map Cost mesures
-       const mesureCostArr = this.state.recipe.ingredients.map((ingredient) => 
-          Number((ingredient.product.price / ingredient.product.format * ingredient.quantity).toFixed(2)))  
+  render() {
+
+    //MAP TH
+    const ingredientsTH = this.state.recipe.ingredients.map((ingredient,index) => (
+      <th key={ingredient.product._id}> Ingredient {index+1} </th>
+    ));
+
+    const MesureTH = this.state.recipe.ingredients.map((ingredient,index) => (
+      <th key={ingredient.product._id}> Mesure {ingredient.product.name} </th>
+    ));
+  
+    const MesureIngredient = this.state.recipe.ingredients.map((ingredient,index) => (
+      <th key={ingredient.product._id}> Cost per {ingredient.product.name}  </th>
+    ));
+
+
+    //Map ingredients
+    const ingredients = this.state.recipe.ingredients.map((ingredient) => (
+      // <tr>
+        <td key={ingredient.product._id}> {ingredient.product.name} </td>
+      // {/* </tr> */}
+    ));
+
+    //Map mesures
+    const mesures = this.state.recipe.ingredients.map((ingredient, index) => (
+    // <tr>
+              <td key={index}>
+        {" "}
+        {ingredient.quantity} {ingredient.product.typeFormat}
+      </td>
+    // {/* </tr> */}
+
+    ));
+    //Map Cost mesures
+    const mesureCostArr = this.state.recipe.ingredients.map((ingredient) =>
+      Number(
+        (
+          (ingredient.product.price / ingredient.product.format) *
+          ingredient.quantity
+        ).toFixed(2)
+      )
+    );
+
+    const mesureCost = mesureCostArr.map((cost, index) => (
+      <td key={index}> {cost} </td>
+    ));
+    //Total recipe Cost
+    const totalCost = mesureCostArr.reduce((a, b) => a + b, 0);
+    //Suggested sell price
+    const suggestedPrice = Math.floor(totalCost * 5 + 1);
+
+    //Redirect a providers
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/recipes" />;
+    }
+
+    return (
+      <div>
+        <Carrousel image={this.state.recipe.recipeImgUrl} title={this.state.recipe.name}/>
+        <div className="recipeDetailsPage">
        
-       const mesureCost = mesureCostArr.map((cost, index) =>
-            <li key={index}> {cost} </li>
-        )
-         //Total recipe Cost 
-        const totalCost = mesureCostArr.reduce((a, b) => a + b, 0)
-        //Suggested sell price
-        const suggestedPrice = Math.floor(totalCost *5 +1)
-        
-    
-
-          //Redirect a providers
-      const { redirect } = this.state;
-      if (redirect) {
-        return <Redirect to='/recipes'/>
-      }
-
-        return (
-<div>
-            <h1>Recipe details</h1>
-            <div className="recipeDetailsPage">
-            <h1 id="recipeTitle">{this.state.recipe.name}</h1> 
-            <img className="cocktailImg" src={this.state.recipe.recipeImgUrl} alt="cocktail img"></img>
-            <h3>Method: {this.state.recipe.method}</h3>
-            <FileUploadRecipes {...this.props} recipe={this.state.recipe}/>
-            <button onClick={this.handleClick} className="button is-danger">Delete recipe</button>
-            <div className="infoContainer">
-            <ul className="ingredients">INGREDIENTS:{ingredients}</ul>
-            <ul className="mesures">MESURES:{mesures}</ul>
-            <ul className="costMesures">COST PER INGREDIENT:{mesureCost}</ul>
-            </div>
-            <div className="stats">
+          <h2>Method: {this.state.recipe.method}</h2>
+          <FileUploadRecipes {...this.props} recipe={this.state.recipe} />
+          <button onClick={this.handleClick} className="button is-danger">
+            Delete recipe
+          </button>
+  <div className="stats">
             <h1>Info:</h1>
             <p>Total cost: {totalCost.toFixed(2)}</p>
             <p>Suggested sell price: {suggestedPrice}</p>
-            </div>
-            </div>
-</div>
-        )
-    }
+          </div>
+        </div>
+
+        {/* TABLA */}
+
+        <div className="table-wrapper">
+          <table className="fl-table">
+            <thead>
+              <tr>
+              {ingredientsTH}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+              {ingredients}
+              </tr>
+            </tbody>
+          </table>
+
+          <table className="fl-table">
+            <thead>
+              <tr>
+              {MesureTH}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+              {mesures}
+              </tr>
+            </tbody>
+          </table>
+
+          <table className="fl-table">
+            <thead>
+              <tr>
+              {MesureIngredient}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+              {mesureCost}
+              </tr>
+            </tbody>
+          </table>
+
+
+          
+        </div>
+      </div>
+    );
+  }
 }
-export default RecipeDetails
+export default RecipeDetails;
