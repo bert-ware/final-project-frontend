cbba11567bca9208c25797e9b9630635da721fa3
Alberto Correia <alberto_cpv@hotmail.com>
2020-09-21 22:58:59 +0200

    recipeDetails fixed

--- src/Components/RecipesDetails/RecipeDetails.js
+++ src/Components/RecipesDetails/RecipeDetails.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Redirect } from "react-router-dom";
-import "./RecipeDetails.css";
+import React, { Component } from "react"
+import axios from "axios"
+import "./RecipeDetails.css"
 import FileUploadRecipes from "../Fileupload/FileUploadRecipes"
+import { Redirect } from 'react-router-dom'
 import Carrousel from "../Carrousel/Carrousel"
+
 
 export class RecipeDetails extends Component {
 
@@ -33,36 +34,33 @@ export class RecipeDetails extends Component {
     axios.delete(process.env.REACT_APP_API_URL +"/recipes/" + this.state.id, {withCredentials: true})
     .then(() => this.setState({ redirect: true }))}
 
-
+render () {
+    //Map ingredients
+    const ingredients = this.state.recipe.ingredients.map((ingredient) =>
+    <li key={ingredient.product._id}> {ingredient.product.name} </li>
+)
     //Map mesures
     const mesures = this.state.recipe.ingredients.map((ingredient, index) => (
       <li key={index}>
-        {" "}
         {ingredient.quantity} {ingredient.product.typeFormat}
       </li>
-    ));
+    ))
     //Map Cost mesures
     const mesureCostArr = this.state.recipe.ingredients.map((ingredient) =>
-      Number(
-        (
-          (ingredient.product.price / ingredient.product.format) *
-          ingredient.quantity
-        ).toFixed(2)
-      )
-    );
+      Number(((ingredient.product.price / ingredient.product.format)*ingredient.quantity).toFixed(2)))
 
     const mesureCost = mesureCostArr.map((cost, index) => (
-      <li key={index}> {cost} </li>
-    ));
+     <li key={index}> {cost} </li>
+        ))
     //Total recipe Cost
-    const totalCost = mesureCostArr.reduce((a, b) => a + b, 0);
+    const totalCost = mesureCostArr.reduce((a, b) => a + b, 0)
     //Suggested sell price
-    const suggestedPrice = Math.floor(totalCost * 5 + 1);
+    const suggestedPrice = Math.floor(totalCost * 5 + 1)
 
     //Redirect a providers
-    const { redirect } = this.state;
+    const { redirect } = this.state
     if (redirect) {
-      return <Redirect to="/recipes" />;
+      return <Redirect to="/recipes" />
     }
 
     return (
@@ -86,7 +84,7 @@ export class RecipeDetails extends Component {
           </div>
         </div>
      </div>   
-    );
+    )
   }
 }
 export default RecipeDetails;
