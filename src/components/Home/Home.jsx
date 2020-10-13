ff088d79554307f6412f4cf0f341a122bd8071ff
Alberto Correia <alberto_cpv@hotmail.com>
2020-10-13 20:52:23 +0200

    redesign home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,7 +13,6 @@ export class Home extends Component {
       <div>
         <Carrousel
           title="We are DrinkApp"
-
           image="./img/drinks/homeslider.png"/>
 
 
@@ -21,10 +20,11 @@ export class Home extends Component {
           <div className="container-fluid">
             <div className="row">
               <div className="col-md-4" >
-                <div className="service-item first-service" id="column1">
-                  <div >
-                    <img id="logoDrink" src="/img/logo-white.svg" alt="" />
-                  </div>
+                <div className="service-item first-service">
+                  
+                      <img id="logoDrink" src="/img/logo-white.svg" alt="" />
+                   
+                  <div>
                   <h4>What is Drinkapp?</h4>
                   <p>
                     Drinkapp is a new tool to calculate cost breakdown of your
@@ -33,84 +33,69 @@ export class Home extends Component {
                     New cocktail menu? No time for excel? Want to know your cost
                     breakdown in a simple way? This is why Drinkapp is for you!
                   </p>
+                  </div>
                 </div>
               </div>
               <div className="col-md-4">
                 <div className="service-item second-service" id="column2">
-                  <div className="icon">
-                    <img className="bottomIcon" src="/img/Profile.svg" alt="" />
-                    <p>
-                      <b>1.-</b> Signup and create your <b>PROFILE</b>
+                  <div className="effectBox">
+                    <div className="box-front1">
+                      <img className="bottomIcon" src="/img/Profile.svg" alt="" />
+                    </div> 
+                    <div className="box-back1">
+                    <p id="centered">
+                      <b>1.-</b> Signup and create your <b>PROFILE</b> for FREE!
                     </p>
+                   </div>
                   </div>
-                  <div className="icon">
+                  <div className="effectBox">
+                  <div className="box-front1">
                     <img
                       className="bottomIcon"
                       src="/img/Providers.svg"
-                      alt=""
-                    />
+                      alt=""/>
+                  </div>
+                  <div className="box-back1">
                     <p>
-
                       <b>2.-</b> Introduce your <b>PROVIDERS</b> In 
                       each of them you can introduce their
-
                       products and the playground will be set!
                     </p>
+                  </div>
                   </div>
                 </div>
               </div>
               <div className="col-md-4">
                 <div className="service-item third-service" id="column3">
-                  <div className="icon">
+                <div className="effectBox">
+                  <div className="box-front2">
                     <img className="bottomIcon" src="/img/Products.svg" alt="" />
-                    <p>
-                      <b>3.-</b> In <b>PRODUCTS</b> page you can search for
-                      ingredients, compare prices from different providers 
-                       and create a new recipe
+                  </div>
+                    <div className="box-back2">  
+                     <p>
+                        <b>3.-</b> In <b>PRODUCTS</b> page you can search for
+                         ingredients, compare prices from different providers 
+                         and create a new recipe
                     </p>
                   </div>
-               
-                  <div className="icon">
+               </div>
+               <div className="effectBox">
+                  <div className="box-front2">
                     <img className="bottomIcon" src="/img/Recipes.svg" alt="" />
+                  </div>
+                  <div className="box-back2">  
                     <p>
                       <b>4.-</b>In <b>RECIPES</b> page  select a  recipe and
                       access  to a detail of ingredients  and total
                        cost break down!
                     </p>
-                  </div>                
+                  </div>
+                </div>                  
                 </div>
               </div>
             </div>
           </div>
         </section>
-
-        {/* <section id="gallerie">
-          <div className="grid_container">
-            <div className="box grid_item">
-              <img
-                className="galerie-2"
-                src="https://scontent-mad1-1.xx.fbcdn.net/v/t31.0-8/28514692_1157581404377630_2337639456659598352_o.jpg?_nc_cat=110&_nc_sid=730e14&_nc_ohc=K3FxEP37DMIAX-Igpsi&_nc_ht=scontent-mad1-1.xx&oh=a3725101897dbca0e1d3f9eeca962925&oe=5F6D2D21"
-                alt="img"
-              />
-            </div>
-
-            <div className="box grid_item">
-              <img
-                className="galerie-2"
-                src="https://scontent-mad1-1.xx.fbcdn.net/v/t31.0-8/15591630_10154805871668498_593361105097038447_o.jpg?_nc_cat=107&_nc_sid=9267fe&_nc_ohc=3aypFyyGPNoAX_rRlHm&_nc_ht=scontent-mad1-1.xx&oh=a1a032eb578d6a3cc2d3c6bd211a703f&oe=5F6F0BFF"
-                alt="img"
-              />
-            </div>
-
-            <div className="box grid_item">
-              <img
-                className="galerie-2"
-                src="https://scontent-mad1-1.xx.fbcdn.net/v/t31.0-8/28619423_1158161937652910_2273450483933074375_o.jpg?_nc_cat=103&_nc_sid=730e14&_nc_ohc=heFaEG0tb-EAX88ys8Z&_nc_ht=scontent-mad1-1.xx&oh=447919e385123fb8e381805d9658510a&oe=5F6D5EF5"
-                alt="img"
-              />
-            </div>
-          </div>
-        </section> */}
         <BottomNavigation />
       </div>
     )
